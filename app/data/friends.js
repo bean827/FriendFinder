ed98e0cbf18c7eccb5750306b279fb3a2b56d08d
Mubin Akhawala <m.akhawala@yahoo.com>
2018-01-04 20:41:37 -0500

    majority code

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+// Array of friends to match survey
+var friends = [{
+        name: "Arnold",
+        photo: 'http://assets.schwarzenegger.com/images/img-2.jpg',
+        scores: [
+            5,
+            2,
+            3,
+            2,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "The Rock",
+        photo: 'https://hypb.imgix.net/image/2017/06/dwayne-the-rock-johnson-is-our-generations-arnold-schwarzenegger-1.jpg?w=1755&q=90&fit=max&auto=compress%2Cformat',
+        scores: [
+            5,
+            4,
+            3,
+            2,
+            3,
+            1,
+            3,
+            5,
+            1,
+            4
+        ]
+    },
+    {
+        name: "Seth Rogen",
+        photo: 'http://media.breitbart.com/media/2017/12/Triggered-640x480.jpg',
+        scores: [
+            2,
+            5,
+            5,
+            4,
+            1,
+            1,
+            3,
+            2,
+            3,
+            2
+        ]
+    },
+    {
+        name: "Kristen Bell",
+        photo: 'http://peopledotcom.files.wordpress.com/2017/11/kristen-bell-2.jpg?crop=90px%2C84px%2C1181px%2C791px&resize=728%2C486',
+        scores: [
+            4,
+            3,
+            2,
+            3,
+            3,
+            3,
+            4,
+            5,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Ralphie May",
+        photo: 'https://www.bleedingcool.com/wp-content/uploads/2017/10/a960ed30c3fcbed8525cacb79eac3d1bd10ca94e.jpg',
+        scores: [
+            1,
+            5,
+            4,
+            4,
+            1,
+            2,
+            4,
+            3,
+            3,
+            3
+        ]
+    }
+]
+
+module.exports = friends;
